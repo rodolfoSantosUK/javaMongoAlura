e0bc5c6f4b41e8a902bfd36e6d1dc4e2d34ee555
rodolfoSantosUK <santoseuro2018@gmail.com>
2020-10-03 08:53:07 -0300

    collection.updateOne( Filters.eq( "nome", "Joao") ,                 new Document("$set", new Document("nome","Joao Silva")));

--- JavaMongo/src/main/java/Principal.java
+++ JavaMongo/src/main/java/Principal.java
@@ -1,7 +1,11 @@
 import com.mongodb.MongoClient;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.Filters;
 import org.bson.Document;
+
+import java.util.Arrays;
+import java.util.Date;
 
 public class Principal {
 
@@ -11,9 +15,31 @@ public class Principal {
 
         MongoClient mongoClient = new MongoClient();
         MongoDatabase mongoDatabase = mongoClient.getDatabase("test");
-        MongoCollection<Document> mongoCollection = mongoDatabase.getCollection("alunos");
-        Document document =  mongoCollection.find().first();
-        System.out.println(document);
+        MongoCollection<Document> collection = mongoDatabase.getCollection("alunos");
+        Document aluno =  collection.find().first();
+        System.out.println(aluno);
+
+        Document novoAluno =  new Document("nome", "Joao")
+                .append("data_nascimento", new Date(2003,10,10))
+                .append("curso",new Document("nome", "Historia"))
+                .append("notas", Arrays.asList(10,9,8))
+                .append("habilidades", Arrays.asList(new Document()
+                                                     .append("nome","Ingles")
+                                                     .append("nível","Básico")
+                                                     ,
+                                                     new Document()
+                                                     .append("nome","Espanhol")
+                                                     .append("nível","Básico")));
+
+        collection.insertOne(novoAluno);
+
+        collection.updateOne( Filters.eq( "nome", "Joao") ,
+                new Document("$set", new Document("nome","Joao Silva")));
+
+
+        collection.deleteOne(Filters.eq("nome", "Joao"));
+
+        mongoClient.close();
     }
 
 
